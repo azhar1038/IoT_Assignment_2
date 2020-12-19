cffba6d52e13f18e4ed19b1709f2644dd5deb366
mdazharuddin1011999 <mdazharuddin1011999@gmail.com>
2020-12-19 13:59:15 +0530

    Final pdf

--- till_looping/1_10.py
+++ till_looping/1_10.py
@@ -1,2 +1,2 @@
-n = list(filter(lambda x: x in ['2','3','5','7'], input("Enter a number: ")))
+n = list(filter(lambda x: x in ['2','3','5','7'], input("Enter number: ")))
 print(sum(int(n[i])*int(n[i+1]) for i in range(0, len(n)-1)))
