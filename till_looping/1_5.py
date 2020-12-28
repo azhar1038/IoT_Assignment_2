9de15c27c59514b95dd4325f8d30b2ac5371f8c3
mdazharuddin1011999 <mdazharuddin1011999@gmail.com>
2020-12-28 20:35:14 +0530

    updated 1_5.py

--- till_looping/1_5.py
+++ till_looping/1_5.py
@@ -1,4 +1,4 @@
 n = input("Enter a number: ")
 evens = [int(i) for i in n if int(i)%2==0 and int(i)%4 != 0]
-odds = [int(i) for i in n if int(i)%2!=0 and int(i)%6!=0]
+odds = [int(i) for i in n if int(i)%2!=0 and int(i)%3!=0]
 print(sum(evens)/len(evens) - sum(odds)/len(odds))
